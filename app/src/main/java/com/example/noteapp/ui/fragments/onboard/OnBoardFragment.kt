170be61cb1cad6e2499ce52621a55df0c7975f85
aijamalorozbekova <orozbekovaaijamal610@gmail.com>
2025-05-29 12:18:25 +0300

    Реолизовала shared

--- app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardFragment.kt
@@ -1,45 +1,58 @@
 package com.example.noteapp.ui.fragments.onboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
-import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentOnBoardBinding
 import com.example.noteapp.ui.adapters.PagerAdapter
-import com.google.android.material.tabs.TabLayoutMediator
+import com.example.noteapp.utils.PreferenceHelper
 
 class OnBoardFragment : Fragment() {
 
-    private lateinit var binding: FragmentOnBoardBinding
+    private val binding by lazy {
+        FragmentOnBoardBinding.inflate(layoutInflater)
+    }
+    companion object {
+        var sharedPreferences = PreferenceHelper()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOnBoardBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkFirstTime()
         initialize()
         setupListeners()
 
         val adapter = PagerAdapter(this)
-            binding.viewPager.adapter = adapter
+        binding.viewPager.adapter = adapter
+        binding.dotsIndicator.attachTo(binding.viewPager)
 
-                binding.dotsIndicator.attachTo(binding.viewPager)
+    }
 
+    private fun checkFirstTime() {
+        sharedPreferences.unit(requireContext())
+        if (sharedPreferences.isShowOnBoard) {
+            val action = OnBoardFragmentDirections.actionOnBoardFragmentToNoteFragment()
+            findNavController().navigate(action)
+            onDestroyView()
+        }
     }
 
     private fun initialize() {
         binding.viewPager.adapter = PagerAdapter(this)
     }
 
-    private fun setupListeners() = with(binding.viewPager) {
+    private fun setupListeners() = with(binding.viewPager){
         registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) = with(binding){
                 super.onPageSelected(position)
@@ -55,5 +68,13 @@ class OnBoardFragment : Fragment() {
                 }
             }
         })
+
+        binding.buttonBegin.setOnClickListener{
+            if (!sharedPreferences.isShowOnBoard){
+                val action = OnBoardFragmentDirections.actionOnBoardFragmentToNoteFragment()
+                findNavController().navigate(action)
+                sharedPreferences.isShowOnBoard = true
+            }
+        }
     }
 }
